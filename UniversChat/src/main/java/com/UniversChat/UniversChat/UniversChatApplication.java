13ebf983eb80cbaf0830b2159f54e86c4de4e31e
JohnMichaelCopain <jeankumba1@gmail.com>
2024-05-27 13:26:43 +0200

    First commit

---
+++ UniversChat/src/main/java/com/UniversChat/UniversChat/UniversChatApplication.java
@@ -0,0 +1,13 @@
+package com.UniversChat.UniversChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversChatApplication.class, args);
+	}
+
+}
---
+++ UniversChat/src/test/java/com/UniversChat/UniversChat/UniversChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UniversChat.UniversChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
